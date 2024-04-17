3861476abd2be71be994393f882effea0a366641
Cameron <cameo221@gmail.com>
2024-04-17 03:40:57 -0700

    changed the tiles finder to get more kinds of tile rectangles. changed it where separate levels are completely deserialized for code simplicity. update to 1.5.3.0

--- Scripts/Export/LDtkAdditionalTilesFinder.cs
+++ Scripts/Export/LDtkAdditionalTilesFinder.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Utf8Json;
 
 namespace ExportTilesetDefinition
 {
@@ -21,61 +23,118 @@ namespace ExportTilesetDefinition
         /// Important: when we get the slices inside levels, we need to dig into json for some of these.
         /// Because we are looking for tile instances in levels, 
         /// Technically we only need to grab the malformed tiles; the ones that are not in equal width/height of the layer's gridsize.
-        /// This is because all of the gridsize tiles are going to be generated regardless.
+        /// This is because all the grid size tiles are going to be generated regardless.
         /// The instances where this can be the case are: editor visual for enum def value, editor visual for entity, and tile fields from levels/entities
+        ///
+        /// Instance-based possible sources of additional sprites are:
+        /// - EntityInstance.Tile
+        /// - EntityInstance.FieldInstances.Tile
+        /// - Level.FieldInstances.Tile
+        ///
+        /// Definition-based possible sources of additional sprites are:
+        /// - AutoLayerRuleGroup.Icon
+        /// - EntityDefinition.TileRect
+        /// - EntityDefinition.UiTileRect
+        /// - EnumValueDefinition.TileRect
+        /// - IntGridValueDefinition.Tile
         /// </summary>
         public Dictionary<int, HashSet<Rectangle>> GetAllAdditionalSpritesInProject(LdtkJson json)
         {
-            Dictionary<int, HashSet<Rectangle>> fieldSlices = new Dictionary<int, HashSet<Rectangle>>();
-
-            foreach (World world in GetWorlds(json))
+            Dictionary<int, HashSet<Rectangle>> allRects = new Dictionary<int, HashSet<Rectangle>>();
+            
+            // - AutoLayerRuleGroup.Icon
+            // - IntGridValueDefinition.Tile
+            foreach (LayerDefinition layer in json.Defs.Layers)
             {
+                foreach (AutoLayerRuleGroup group in layer.AutoRuleGroups)
+                {
+                    AddToDict(group.Icon);
+                }
+                foreach (IntGridValueDefinition value in layer.IntGridValues)
+                {
+                    AddToDict(value.Tile);
+                }
+            }
+            
+            // - EntityDefinition.TileRect
+            // - EntityDefinition.UiTileRect
+            foreach (EntityDefinition def in json.Defs.Entities)
+            {
+                AddToDict(def.TileRect);
+                AddToDict(def.UiTileRect);
+            }
+            
+            // - EnumValueDefinition.TileRect
+            foreach (var def in json.Defs.Enums.Concat(json.Defs.ExternalEnums))
+            {
+                foreach (EnumValueDefinition value in def.Values)
+                {
+                    AddToDict(value.TileRect);
+                }
+            }
+            
+            //Instances
+            World[] worlds = GetWorlds(json);
+            foreach (World world in worlds)
+            {
+                if (json.ExternalLevels)
+                {
+                    for (int i = 0; i < world.Levels.Length; i++)
+                    {
+                        world.Levels[i] = GetSeparateLevel(world.Levels[i]);
+                    }
+                    Console.WriteLine();
+                    
+                    Level GetSeparateLevel(Level srcLvl)
+                    {
+                        Stopwatch stopWatch = Stopwatch.StartNew();
+                        Level lvl;
+                        try
+                        {
+                            string levelPath = GetPathRelativeToPath(_projectPath, srcLvl.ExternalRelPath);
+                            byte[] levelJson = File.ReadAllBytes(levelPath);
+                            lvl = JsonSerializer.Deserialize<Level>(levelJson);
+                        }
+                        catch (Exception e)
+                        {
+                            throw new JsonParsingException($"Failed to load separate level \"{srcLvl.Identifier}\": {e}");
+                        }
+                        finally
+                        {
+                            stopWatch.Stop();
+                        }
+                        
+                        Console.WriteLine($"Deserialize separate level \"{srcLvl.Identifier}\" in {stopWatch.Elapsed.Milliseconds} ms");
+                        return lvl;
+                    }
+                }
+                
+                // - EntityInstance.Tile
+                // - EntityInstance.FieldInstances.Tile
+                // - Level.FieldInstances.Tile
                 foreach (Level level in world.Levels)
                 {
-                    //Entity tile fields. If external levels, then dig into the level. If in our own json, then we can safely get them from the layer instances in the json.
-                    if (json.ExternalLevels)
+                    //level fields
+                    foreach (FieldInstance levelFieldInstance in level.FieldInstances) 
                     {
-                        string levelPath = GetPathRelativeToPath(_projectPath, level.ExternalRelPath);
-
-                        List<FieldInstance> fields = new List<FieldInstance>();
-                        if (!JsonDigger.GetUsedFieldTiles(levelPath, ref fields))
-                        {
-                            Console.WriteLine($"Couldn't get entity tile field instance for level: {level.Identifier}");
-                            continue;
-                        }
-                    
-                        foreach (FieldInstance field in fields)
-                        {
-                            TryAddFieldInstance(field);
-                        }
-                        continue;
+                        TryAddFieldInstance(levelFieldInstance);
                     }
-
-                    //NOTICE: depending on performance from directly getting json data instead of digging, i'll release this back.
-                    //else it's not external levels and can ge grabbed from the json data for better performance
-                    //Level field instances are still available in project json even with separate levels. They are both available in project and separate level files
-                    TryAddFieldInstancesFromJsonData();
-                    void TryAddFieldInstancesFromJsonData()
+                    
+                    //entity tile and entity fields
+                    foreach (LayerInstance layer in level.LayerInstances)
                     {
-                        //level's field instances
-                        foreach (FieldInstance levelFieldInstance in level.FieldInstances) 
+                        foreach (EntityInstance entity in layer.EntityInstances)
                         {
-                            TryAddFieldInstance(levelFieldInstance);
-                        }
-                
-                        //entity field instances
-                        foreach (LayerInstance layer in level.LayerInstances)
-                        {
-                            foreach (EntityInstance entity in layer.EntityInstances)
+                            AddToDict(entity.Tile);
+                            
+                            foreach (FieldInstance entityField in entity.FieldInstances)
                             {
-                                foreach (FieldInstance entityField in entity.FieldInstances)
-                                {
-                                    TryAddFieldInstance(entityField);
-                                }
+                                TryAddFieldInstance(entityField);
                             }
                         }
                     }
-                    
+                    continue;
+
                     void TryAddFieldInstance(FieldInstance field)
                     {
                         if (!IsTile(field))
@@ -98,19 +157,63 @@ namespace ExportTilesetDefinition
                             }
 
                             //deny adding duplicated to avoid identifier uniqueness
-                            int key = rect.TilesetUid;
-                            if (!fieldSlices.ContainsKey(key))
+                            AddToDict(rect);
+                        }
+                    }
+                    
+                    void DoSomeSeparateLevelLoading()
+                    {
+                        //NOTICE: depending on performance from directly getting json data instead of digging, i'll release this back.
+                        //else it's not external levels and can ge grabbed from the json data for better performance
+                        //Level field instances are still available in project json even with separate levels. They are both available in project and separate level files
+                        //level's field instances
+                        //discarded for the time being. look back on it if there's performance concerns
+                        
+                        //Entity tile fields. If external levels, then dig into the level. If in our own json, then we can safely get them from the layer instances in the json.
+                        if (json.ExternalLevels)
+                        {
+                            string levelPath = GetPathRelativeToPath(_projectPath, level.ExternalRelPath);
+
+                            List<FieldInstance> fields = new List<FieldInstance>();
+                        
+                            //TEMP: don't use the digger for now just to make things simpler for now. if the export app gets slow, then we can reconsider
+                            if (!JsonDigger.GetUsedFieldTiles(levelPath, ref fields))
                             {
-                                fieldSlices.Add(key, new HashSet<Rectangle>());
+                                Console.WriteLine($"Couldn't get entity tile field instance for level: {level.Identifier}");
+                                //continue;
                             }
-                            fieldSlices[key].Add(FromTilesetRectangle(rect));
+
+                        
+                            foreach (FieldInstance field in fields)
+                            {
+                                TryAddFieldInstance(field);
+                            }
+                        
+                            //continue;
                         }
                     }
                 }
             }
-            return fieldSlices;
+            return allRects;
+
+            void AddToDict(TilesetRectangle rect)
+            {
+                if (rect == null)
+                {
+                    return;
+                }
+                
+                var key = rect.TilesetUid;
+                if (!allRects.ContainsKey(key))
+                {
+                    allRects.Add(key, new HashSet<Rectangle>());
+                }
+                allRects[key].Add(FromTilesetRectangle(rect));
+            }
+            
+            
         }
-        
+
         public string GetPathRelativeToPath(string assetPath, string relPath)
         {
             string startDir = Path.GetDirectoryName(assetPath);
